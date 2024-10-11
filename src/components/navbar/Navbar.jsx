1b1649f117d3c4f5cdb9a20ca328848e616be3a1
Ljubomir <ljupcedavitkov14@gmail.com>
2024-10-11 16:35:51 +0200

    Responsive hamburger menu for small devices

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,11 @@
 import "./navbar.scss"
+import {useState} from "react";
 
-function Navbar (){
+function Navbar() {
+
+    const [open, setOpen] = useState(false)
+
+
     return (
         <nav>
             <div className="left">
@@ -16,6 +21,19 @@ function Navbar (){
             <div className="right">
                 <a href="/">Sign in</a>
                 <a className="register" href="/">Sign up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png"
+                         alt=""
+                         onClick={() => setOpen((prev) => !prev)}/>
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign in</a>
+                    <a href="/">Sign up</a>
+                </div>
             </div>
         </nav>
     )
